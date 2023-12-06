fc0eac9ff9b26e8fcf9139310c311468a15092da
Mantas <mantas.varnagiris@gmail.com>
2023-12-06 14:02:34 +0200

    Added time measuring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,18 @@
+import kotlin.system.measureTimeMillis
+
 fun main(args: Array<String>) {
-//    println("Day 1 Part 1 = ${day1Part1()}")
-//    println("Day 1 Part 2 = ${day1Part2()}")
-//    println("Day 2 Part 1 = ${day2Part1()}")
-//    println("Day 2 Part 2 = ${day2Part2()}")
-//    println("Day 3 Part 1 = ${day3Part1()}")
-//    println("Day 3 Part 2 = ${day3Part2()}")
-//    println("Day 4 Part 1 = ${day4Part1()}")
-//    println("Day 4 Part 2 = ${day4Part2()}")
-    println("Day 5 Part 1 = ${day5Part1()}")
-    println("Day 5 Part 2 = ${day5Part2()}")
+    measured { print("Day 1 Part 1 = ${day1Part1()}") }
+    measured { print("Day 1 Part 2 = ${day1Part2()}") }
+    measured { print("Day 2 Part 1 = ${day2Part1()}") }
+    measured { print("Day 2 Part 2 = ${day2Part2()}") }
+    measured { print("Day 3 Part 1 = ${day3Part1()}") }
+    measured { print("Day 3 Part 2 = ${day3Part2()}") }
+    measured { print("Day 4 Part 1 = ${day4Part1()}") }
+    measured { print("Day 4 Part 2 = ${day4Part2()}") }
+    measured { print("Day 5 Part 1 = ${day5Part1()}") }
+    measured { print("Day 5 Part 2 = ${day5Part2()}") }
+}
+
+private fun measured(code: () -> Unit) {
+    measureTimeMillis { code() }.also { print(" | In: ${it}ms\n") }
 }
